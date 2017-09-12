d81e9a3d976bfa445f2583748074965b6d3d13ef
Kritika <kritikagr12@gmail.com>
2017-09-13 00:53:24 +0530

    basic functions working

--- tuple.java
+++ tuple.java
@@ -1,8 +1,9 @@
 import java.util.*;
 import java.text.*;
+import java.io.Serializable ;
 
 //class for return values
-public class tuple
+public class tuple implements Serializable 
 {
   private Integer tid;
   private Integer balance;
